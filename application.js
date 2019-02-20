d581dcce551e01e09c7c9c81f68b6430f54a1e36
Michel CORNE <mcorne@akliagroupe.lan>
2019-02-20 15:36:52 +0100

    fixed layout

--- application.js
+++ application.js
@@ -1,9 +1,22 @@
 /**
+ * @see https://stackoverflow.com/questions/23451611/is-there-anyway-to-have-a-textarea-autofit-height-based-on-the-content-at-page
+ */
+function autofit_textarea_height()
+{
+    var element = document.getElementById('code');
+
+    if (element.clientHeight < element.scrollHeight) {
+        element.style.height = element.scrollHeight + 2 + "px";
+    } 
+}
+
+/**
  * 
  * @param {Event} e 
  * @see https://twinnation.org/articles/10/replace-tab-by-4-spaces-in-textarea-or-text-input
  */
-function handle_tab_character(e) {
+function handle_tab_character(e) 
+{
     if (e.key === "Tab") {
         e.preventDefault();
         let start = e.target.selectionStart;
