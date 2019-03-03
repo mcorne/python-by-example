9710f5e49a138621bc3d3e93c9f8ff1eeef9913e
Michel Corne <mcorne@yahoo.com>
2019-03-03 18:17:19 +0100

    added functions

--- docstrings.js
+++ docstrings.js
@@ -1,6 +1,15 @@
 var docstrings = {
     abs:
 "Return the absolute value of the argument.",
+    dir:
+"dir([object]) -> list of strings\n\
+\n\
+If called without an argument, return the names in the current scope.\n\
+Else, return an alphabetized list of names comprising (some of) the attributes of the given object, and of attributes reachable from it.\n\
+If the object supplies a method named __dir__, it will be used; otherwise the default dir() logic is used and returns:\n\
+- for a module object: the module's attributes.\n\
+- for a class object:  its attributes, and recursively the attributes of its bases.\n\
+- for any other object: its attributes, its class's attributes, and recursively the attributes of its class's base classes.",
     print:
 "print(value, ..., sep=' ', end='\\n', file=sys.stdout, flush=False)\n\
 \n\
--- examples.js
+++ examples.js
@@ -1,8 +1,20 @@
 var examples = {
     abs: [
-        {short: "abs(123)"          , full: "print(abs(123))"},
-        {short: "abs(-456)"         , full: "print(abs(-456))"},
-        {short: "abs(complex(3, 4))", full: "print(abs(complex(3, 4)))"}
+        {short: "abs(123)"                        , full: "print(abs(123))"},
+        {short: "abs(-456)"                       , full: "print(abs(-456))"},
+        {short: "abs(complex(3, 4))"              , full: "print(abs(complex(3, 4)))"}
+    ],
+    dir: [
+        {short: "dir()"                           , full: "print(dir())"},
+        {short: "dir(math)"                       , full: "import math\nprint(dir(math))"},
+        {short: "dir(str)"                        , full: "import math\nprint(dir(str))"},
+        {short: "dir(object)"                     , full:
+"class Shape:\n\
+    def __dir__(self):\n\
+        return ['area', 'perimeter', 'location']\n\
+s = Shape()\n\
+print(dir(s))"
+        }
     ],
     print: [
         {short: "print('Hello!')"                 , full: "print('Hello!')"},
@@ -10,6 +22,6 @@ var examples = {
         {short: "print(1, end='-')..."            , full: "print(1, end='-')\nprint(2, end='-')\nprint(3)"}
     ],
     "str.capitalize": [
-        {short: "'hello world!'.capitalize()", full: "print('hello world!'.capitalize())"}
+        {short: "'hello world!'.capitalize()"     , full: "print('hello world!'.capitalize())"}
     ]
 }
